fc394127fd4fb0bc0c03316b32f524570bbe5635
Alvin <alvinallen333@gmail.com>
2021-07-15 16:40:50 +0530

    Removed intent and added greet

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands, tasks
 import json
-intent=discord.Intents.default()
-intent.members=True
+import random
+import os
 default_prefix="h!"
 prefix={}
-client=commands.Bot(command_prefix=default_prefix,intents=intent)
+client=commands.Bot(command_prefix=default_prefix)
 @client.command(aliases=['p'])
 async def ping(ctx):
     await ctx.send("Pong\nLatency: "+client.latency)
 @client.command(aliases=["hi","hello","hey"])
 async def greetings(ctx):   
     greet_msgs = ["Hi {}!".format(ctx.author.name), "Hey {}!".format(ctx.author.name), "How are you {}?".format(ctx.author.name), "How's it going {}?".format(ctx.author.name)]
+    await ctx.send(random.choice(greet_msgs))
 
-client.run("token")
+
+client.run("ODU4NDA1MTUyMjQyNDAxMzMw.YNdqGA.hK9OJTuSyz4pG3fMP4-cQFwLblQ")
