a949fee7ecf057c1dfa5b8df6b29a5894ec70b9e
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-07-15 16:30:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import discord
 from discord.ext import commands, tasks
 import json
-import os
 intent=discord.Intents.default()
+intent.members=True
 default_prefix="h!"
 prefix={}
-client=commands.Bot(command_prefix=default_prefix)
+client=commands.Bot(command_prefix=default_prefix,intents=intent)
 @client.command(aliases=['p'])
 async def ping(ctx):
-    await ctx.send("Pong\nLatency: "+str(client.latency*1000))
+    await ctx.send("Pong\nLatency: "+client.latency)
+@client.command(aliases=["hi","hello","hey"])
+async def greetings(ctx):   
+    greet_msgs = ["Hi {}!".format(ctx.author.name), "Hey {}!".format(ctx.author.name), "How are you {}?".format(ctx.author.name), "How's it going {}?".format(ctx.author.name)]
 
 client.run("token")
