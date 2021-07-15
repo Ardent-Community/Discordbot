2b8a782d1a5c10e98df7fb630c23ba25747fb740
Alvin <alvinallen333@gmail.com>
2021-07-15 17:55:33 +0530

    Added insta feature

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import discord
 from discord.ext import commands, tasks
 import json
 import random
+from instagramy import *
 import os
 default_prefix="h!"
+color_var=discord.Color.from_rgb(0, 235, 0)
 prefix={}
 client=commands.Bot(command_prefix=default_prefix)
 @client.command(aliases=['p'])
@@ -16,12 +18,22 @@ async def greetings(ctx):
 client.remove_command("help")
 @client.command(aliases=["use",'help','info'])
 async def help_menu(ctx):
-    embed = discord.Embed(title="Command Menu", color=discord.Color.from_rgb(0, 235, 0))
+    embed = discord.Embed(title="Command Menu", color=color_var)
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/858234706305482785/865191551444844544/hackathonlogo.png")
     embed.add_field(name="Social",value="h!insta to get insta feed\nh!tweet to get twitter feed")
     embed.add_field(name="Events", value="h!hdt to get hackathon dates")
     embed.add_field(name="Questions", value="h!FAQ to drop your questions and our team will answer")
     embed.add_field(name="Addtional Queries", value="`ansh@econhacks.org`")
+    await ctx.send(embed=embed)
+@client.command()
+async def insta(ctx):
+    user=InstagramUser("testforhackathonbot")
+    print(user)
+    descript=user.posts[0].caption
+    thumb=user.profile_picture_url
+    embed=discord.Embed(title="Insta",description=descript, color=color_var)
+    embed.set_image(url=user.posts[0].post_source)
+    embed.set_thumbnail(url=thumb)
     await ctx.send(embed=embed)
     
 file = open("../env.txt","r")
